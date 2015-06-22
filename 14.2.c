5c2ad242510d49022dc818ec4092695fac2e7c11
wuzhouhui <wuzhouhui250@gmail.com>
2015-06-22 10:50:05 +0800

    completes exercise 14.2

---
+++ 14.2.c
@@ -0,0 +1,52 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  connect_timeo.c
+ *
+ *    Description:  connect() with timeout.
+ *
+ *        Version:  1.0
+ *        Created:  03/17/2015 09:53:33 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+#include "unp.h"
+
+static void connect_alarm(int);
+
+int connect_timeo(int sockfd, const SA *saptr, socklen_t salen, int nsec)
+{
+	Sigfunc *sigfunc;
+	int	n, left;
+
+	if ((sigfunc = signal(SIGALRM, connect_alarm)) == SIG_ERR)
+		err_sys("connect_timeo: signal error");
+	if ((left = alarm(nsec)) != 0)
+		err_msg("connect_timeo: alarm was already set");
+	if ((n = connect(sockfd, saptr, salen)) < 0) {
+		close(sockfd);
+		if (errno = EINTR)
+			errno = ETIMEDOUT;
+	}
+	alarm(0);	/* turn off the alarm */
+
+	/*
+	 * restore previous signal handler.
+	 */
+	if (signal(SIGALRM, sigfunc) == SIG_ERR)
+		err_sys("connect_time: signal error");
+	alarm(left);	/* restore previous alarm */
+
+	return n;
+}
+
+static void connect_alarm(int signo)
+{
+	return;	/* just interrupt the connect() */
+}
